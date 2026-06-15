85c19c199b41d3ca49d87d80d4d22bba2c27bbfe
victor141516 <victor.fernandez.gabriel@gmail.com>
2026-06-15 12:27:47 +0200

    Redesign app icon as lime speaker

--- tools/IconBuilder/Program.cs
+++ tools/IconBuilder/Program.cs
@@ -13,12 +13,12 @@ var sizes = new[] { 16, 20, 24, 32, 40, 48, 64, 128, 256 };
 var frames = new List<(int Size, byte[] Png)>();
 foreach (var size in sizes)
 {
-    using var image = DrawBoLime(size);
+    using var image = DrawVoolimeIcon(size);
     frames.Add((size, EncodePng(image)));
 }
 
 WriteIco(iconPath, frames);
-using (var preview = DrawBoLime(512))
+using (var preview = DrawVoolimeIcon(512))
 {
     preview.Save(previewPath, ImageFormat.Png);
 }
@@ -26,7 +26,7 @@ using (var preview = DrawBoLime(512))
 Console.WriteLine(iconPath);
 Console.WriteLine(previewPath);
 
-static Bitmap DrawBoLime(int size)
+static Bitmap DrawVoolimeIcon(int size)
 {
     var bitmap = new Bitmap(size, size, PixelFormat.Format32bppArgb);
     using var graphics = Graphics.FromImage(bitmap);
@@ -37,59 +37,76 @@ static Bitmap DrawBoLime(int size)
     graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
     graphics.ScaleTransform(size / 256f, size / 256f);
 
-    using var shadow = new SolidBrush(Color.FromArgb(48, 0, 0, 0));
-    graphics.FillEllipse(shadow, 38, 174, 144, 18);
+    var speakerBounds = Rect(25, 47, 160, 160);
+    var pulpBounds = Rect(45, 67, 120, 120);
+    var center = new PointF(105, 127);
 
-    var limeOuter = Rect(34, 39, 154, 154);
-    using var outerBrush = new LinearGradientBrush(limeOuter, FromHtml("#56C83F"), FromHtml("#208E45"), 110f);
-    using var outerPen = new Pen(FromHtml("#125C34"), 7f) { LineJoin = LineJoin.Round };
-    graphics.FillEllipse(outerBrush, limeOuter);
-    graphics.DrawEllipse(outerPen, limeOuter);
+    using var shadow = new SolidBrush(Color.FromArgb(50, 0, 0, 0));
+    graphics.FillEllipse(shadow, 38, 192, 136, 18);
 
-    var limeInner = Rect(52, 57, 118, 118);
-    using var innerBrush = new LinearGradientBrush(limeInner, FromHtml("#E4FF79"), FromHtml("#91DF39"), 90f);
-    using var innerPen = new Pen(Color.FromArgb(210, 255, 255, 232), 5f);
-    graphics.FillEllipse(innerBrush, limeInner);
-    graphics.DrawEllipse(innerPen, limeInner);
+    DrawWave(graphics, Rect(166, 92, 43, 70), 9f);
+    DrawWave(graphics, Rect(176, 75, 65, 104), 9f);
 
-    using var segmentPen = new Pen(Color.FromArgb(145, 32, 111, 42), 4f)
+    using var outerBrush = new LinearGradientBrush(speakerBounds, FromHtml("#58D34A"), FromHtml("#14723B"), 115f);
+    using var outerPen = new Pen(FromHtml("#0E5132"), 8f) { LineJoin = LineJoin.Round };
+    graphics.FillEllipse(outerBrush, speakerBounds);
+    graphics.DrawEllipse(outerPen, speakerBounds);
+
+    using var rimPen = new Pen(Color.FromArgb(230, 228, 255, 175), 7f);
+    graphics.DrawEllipse(rimPen, Rect(39, 61, 132, 132));
+
+    using var pulpClip = new GraphicsPath();
+    pulpClip.AddEllipse(pulpBounds);
+    var previousClip = graphics.Clip;
+    graphics.SetClip(pulpClip);
+
+    var segmentColors = new[]
+    {
+        FromHtml("#DFFF67"),
+        FromHtml("#B9F143"),
+        FromHtml("#91DB35"),
+        FromHtml("#CFFB55")
+    };
+    for (var i = 0; i < 10; i++)
+    {
+        using var segmentBrush = new SolidBrush(segmentColors[i % segmentColors.Length]);
+        graphics.FillPie(segmentBrush, pulpBounds, -90 + i * 36, 36);
+    }
+
+    graphics.Clip = previousClip;
+
+    using var pulpPen = new Pen(Color.FromArgb(230, 247, 255, 203), 5f);
+    graphics.DrawEllipse(pulpPen, pulpBounds);
+
+    using var segmentPen = new Pen(Color.FromArgb(160, 42, 118, 43), 4f)
     {
         StartCap = LineCap.Round,
         EndCap = LineCap.Round
     };
-    var center = new PointF(111, 116);
-    foreach (var angle in new[] { -70, -25, 20, 65, 110, 155 })
+    for (var i = 0; i < 10; i++)
     {
+        var angle = -90 + i * 36;
         var radians = Math.PI * angle / 180d;
         var end = new PointF(
-            center.X + (float)Math.Cos(radians) * 55f,
-            center.Y + (float)Math.Sin(radians) * 55f);
+            center.X + (float)Math.Cos(radians) * 59f,
+            center.Y + (float)Math.Sin(radians) * 59f);
         graphics.DrawLine(segmentPen, center, end);
     }
 
-    using var glint = new SolidBrush(Color.FromArgb(140, 255, 255, 255));
-    graphics.FillEllipse(glint, 73, 68, 34, 18);
-
-    using var bodyBrush = new LinearGradientBrush(Rect(121, 86, 82, 91), FromHtml("#263442"), FromHtml("#111923"), 0f);
-    using var bodyPen = new Pen(Color.FromArgb(235, 9, 16, 25), 6f) { LineJoin = LineJoin.Round };
-    using var speakerPath = new GraphicsPath();
-    speakerPath.AddPolygon(new[]
+    using var coneBrush = new PathGradientBrush(pulpClip)
     {
-        new PointF(121, 108),
-        new PointF(148, 108),
-        new PointF(190, 76),
-        new PointF(190, 180),
-        new PointF(148, 148),
-        new PointF(121, 148)
-    });
-    graphics.FillPath(bodyBrush, speakerPath);
-    graphics.DrawPath(bodyPen, speakerPath);
+        CenterColor = Color.FromArgb(95, 8, 34, 26),
+        SurroundColors = [Color.FromArgb(0, 8, 34, 26)]
+    };
+    graphics.FillEllipse(coneBrush, pulpBounds);
 
-    using var grilleBrush = new SolidBrush(Color.FromArgb(110, 255, 255, 255));
-    graphics.FillRoundedRectangle(grilleBrush, Rect(132, 118, 18, 20), 5);
+    using var capBrush = new LinearGradientBrush(Rect(83, 105, 44, 44), FromHtml("#1D4836"), FromHtml("#0B2119"), 90f);
+    using var capPen = new Pen(Color.FromArgb(225, 229, 255, 188), 4f);
+    graphics.FillEllipse(capBrush, Rect(83, 105, 44, 44));
+    graphics.DrawEllipse(capPen, Rect(83, 105, 44, 44));
 
-    DrawWave(graphics, Rect(177, 94, 42, 68), 9f);
-    DrawWave(graphics, Rect(163, 76, 73, 104), 9f);
+    using var glint = new SolidBrush(Color.FromArgb(145, 255, 255, 255));
+    graphics.FillEllipse(glint, 66, 72, 35, 19);
 
     return bitmap;
 }
